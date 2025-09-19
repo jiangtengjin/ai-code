e6d7febc0e4db13c48ea5102d0ddb394ccf41a93
jiangtengjin <jiangtengjin@appiron.cn>
2025-09-19 13:40:40 +0800

    feat:后端项目初始化

---
+++ src/main/java/com/xhh/aicode/AiCodeApplication.java
@@ -0,0 +1,15 @@
+package com.xhh.aicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy(exposeProxy = true)
+@SpringBootApplication
+public class AiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeApplication.class, args);
+    }
+
+}
