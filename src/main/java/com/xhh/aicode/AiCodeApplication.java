5c4c62c9000c846955fb51d5db81544b7124d668
jiangtengjin <jiangtengjin@appiron.cn>
2025-09-20 17:29:32 +0800

    feat:完成用户模块相关代码的编写

--- src/main/java/com/xhh/aicode/AiCodeApplication.java
+++ src/main/java/com/xhh/aicode/AiCodeApplication.java
@@ -1,10 +1,12 @@
 package com.xhh.aicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.xhh.aicode.mapper")
 @SpringBootApplication
 public class AiCodeApplication {
 
