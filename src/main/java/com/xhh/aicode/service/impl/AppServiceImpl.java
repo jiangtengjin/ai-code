a91eb5578db39824a936e060eb3db26d06e7d364
jiangtengjin <jiangtengjin@appiron.cn>
2025-10-21 14:54:13 +0800

    feat: 可观测性
    - 引入 prometheus 相关依赖以及添加配置
    - 编写监听上下文以及holder
    - 编写指标监听器
    - AI 服务添加监听器

--- src/main/java/com/xhh/aicode/service/impl/AppServiceImpl.java
+++ src/main/java/com/xhh/aicode/service/impl/AppServiceImpl.java
@@ -28,6 +28,8 @@ import com.xhh.aicode.model.enums.ChatHistoryMessageTypeEnum;
 import com.xhh.aicode.model.enums.CodeGenTypeEnum;
 import com.xhh.aicode.model.vo.AppVO;
 import com.xhh.aicode.model.vo.UserVO;
+import com.xhh.aicode.monitor.MonitorContext;
+import com.xhh.aicode.monitor.MonitorContextHolder;
 import com.xhh.aicode.service.AppService;
 import com.xhh.aicode.service.ChatHistoryService;
 import com.xhh.aicode.service.ScreenshotService;
@@ -94,14 +96,22 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
         String codeGenType = app.getCodeGenType();
         CodeGenTypeEnum codeGenTypeEnum = CodeGenTypeEnum.getEnumByValue(codeGenType);
         ThrowUtils.throwIf(codeGenTypeEnum == null, ErrorCode.SYSTEM_ERROR, "不支持的应用生成类型");
-
         // 5. 先保存用户消息
         chatHistoryService.addChatMessage(appId, message, ChatHistoryMessageTypeEnum.USER.getValue(), loginUser.getId());
-
-        // 6. 调用 AI 生成代码（流式），并在完成或错误时持久化 AI 消息或错误信息
+        // 6. 设置监控上下文
+        MonitorContextHolder.setContext(MonitorContext.builder()
+                .userId(loginUser.getId().toString())
+                .appId(appId.toString())
+                .build()
+        );
+        // 7. 调用 AI 生成代码（流式），并在完成或错误时持久化 AI 消息或错误信息
         Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream(message, codeGenTypeEnum, appId);
-        // 7. 收集 AI 响应内容并在完成后记录到对话历史
-        return streamHandlerExecutor.doExecute(codeStream, chatHistoryService, appId, loginUser, codeGenTypeEnum);
+        // 8. 收集 AI 响应内容并在完成后记录到对话历史
+        return streamHandlerExecutor.doExecute(codeStream, chatHistoryService, appId, loginUser, codeGenTypeEnum)
+                .doFinally(signalType -> {
+                    // 流结束时清理（无论成功/失败/取消）
+                    MonitorContextHolder.clearContext();
+                });
     }
 
     @Override
